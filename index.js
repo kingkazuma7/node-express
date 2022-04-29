9b20e1025a32095d79ce4ac36a3699e1dde03840
kingkazuma7 <your.nasionasio7@gmail.com>
2022-04-29 12:41:59 +0900

    52%まで　post

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 
 const express = require("express");
 const app = express();
+// const coronaData = require("./coronaData.json");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send({
-    "name": "平岡",
-    "age": "45"
-  });
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/autumn", function (req, res) {
+  console.log("reqの中身", req.body);
 });
 
 // バックエンドサーバーとブラウザをつなぐ
