4285825b2d1051ee80c77a495e1df6b81418c000
kingkazuma7 <your.nasionasio7@gmail.com>
2022-05-01 12:40:17 +0900

    2章まで コード

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 const fs = require('fs');
 const express = require("express");
+const activities = require("./activities.json");
 const app = express();
 // const coronaData = require("./coronaData.json");
 app.use(express.urlencoded({ extended: true }));
 
+/* ----- index読み込み ----- */
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
+/* ----- autumnファイル書き込み（投稿） ----- */
 app.post("/autumn", function (req, res) {
   fs.writeFile(__dirname + "/data.txt", req.body.activity, function() {
     res.send("投稿完了");
   });
 });
 
-// バックエンドサーバーとブラウザをつなぐ
+/* ----- データの変更 ----- */
+app.post("/update", function (req, res) {
+  activities[0].activity = req.body.updateActivity;
+  res.send(activities);
+});
+
+/* ----- データの削除 ----- */
+app.post("/delete", function (req, res) {
+  activities.splice(req.body.number, 1);
+  res.send(activities);
+})
+
+/* ----- バックエンドサーバーとブラウザをつなぐ ----- */
 app.listen(5000, function() {
   console.log("Listeningonlocalhostport5000");
 })
